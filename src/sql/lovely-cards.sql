904f8824d486a0710bf9762adb5e629166841822
Ricardo-we <totgamer75@gmail.com>
2022-02-25 09:25:53 -0600

    removed sql comments

--- src/sql/lovely-cards.sql
+++ src/sql/lovely-cards.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 23-02-2022 a las 05:50:22
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `lovely-cards`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cards`
---
 
 CREATE TABLE IF NOT EXISTS `cards` (
   `id` int(11) NOT NULL,
@@ -34,31 +9,12 @@ CREATE TABLE IF NOT EXISTS `cards` (
   `card_name` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `cards`
---
------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `card_content`
---
-
 CREATE TABLE IF NOT EXISTS `card_content` (
   `id` int(11) NOT NULL,
   `card_id` int(11) NOT NULL,
   `heading` varchar(200) NOT NULL,
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `card_content`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `card_images`
---
 
 CREATE TABLE IF NOT EXISTS `card_images` (
   `id` int(11) NOT NULL,
@@ -67,16 +23,6 @@ CREATE TABLE IF NOT EXISTS `card_images` (
   `image_id` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `card_images`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `user`
---
-
 CREATE TABLE IF NOT EXISTS `user`(
   `id` int(11) NOT NULL,
   `username` varchar(230) NOT NULL,
@@ -84,17 +30,6 @@ CREATE TABLE IF NOT EXISTS `user`(
   `password` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `user`
---
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `cards`
---
 ALTER TABLE `cards`
   ADD PRIMARY KEY (`id`);
 
@@ -105,64 +40,28 @@ ALTER TABLE `card_content`
   ADD PRIMARY KEY (`id`),
   ADD KEY `card_content_ibfk_1` (`card_id`);
 
---
--- Indices de la tabla `card_images`
---
 ALTER TABLE `card_images`
   ADD PRIMARY KEY (`id`),
   ADD KEY `card_images_ibfk_1` (`card_id`);
 
---
--- Indices de la tabla `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `cards`
---
 ALTER TABLE `cards`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
---
--- AUTO_INCREMENT de la tabla `card_content`
---
 ALTER TABLE `card_content`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
---
--- AUTO_INCREMENT de la tabla `card_images`
---
 ALTER TABLE `card_images`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
---
--- AUTO_INCREMENT de la tabla `user`
---
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `card_content`
---
 ALTER TABLE `card_content`
   ADD CONSTRAINT `card_content_ibfk_1` FOREIGN KEY (`card_id`) REFERENCES `cards` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `card_images`
---
 ALTER TABLE `card_images`
   ADD CONSTRAINT `card_images_ibfk_1` FOREIGN KEY (`card_id`) REFERENCES `cards` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
